c9239098a9b2e31978dabe60c303169143130921
KMatveychikov <matv00@gmail.com>
2023-05-14 17:52:16 +0300

    update

---
+++ src/main/java/ru/matvey/springrailwayfactory/model/AcceptTrainList.java
@@ -0,0 +1,2 @@
+package ru.matvey.springrailwayfactory.model;public class AcceptTrainList {
+}
---
+++ src/main/java/ru/matvey/springrailwayfactory/repository/AcceptTrainListRepository.java
@@ -0,0 +1,2 @@
+package ru.matvey.springrailwayfactory.repository;public interface AcceptTrainListRepository {
+}
